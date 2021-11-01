106903a99f70aa143496ae0222a5e6f6261a36f1
NicoADavite <nicolasalceodavite@gmail.com>
2021-11-01 13:08:00 -0300

    agregando app.get para enviar las distintas views y creacion de archivos css

--- proyecto_marketplace/app.js
+++ proyecto_marketplace/app.js
@@ -1,16 +1,35 @@
+// Require de Express y Path
 const express = require('express');
 const app = express();
-
 const path = require('path');
 
-const publicPath = path.resolve(__dirname, "./public");
+// Definimos la carpeta public para acceder a los elementos estaticos
 
-app.use(express.static(publicPath))
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+// lineas de codigo app.get para recibir las distintas views de la página
 
 app.get("/",(req,res) => {
     res.sendFile(path.resolve(__dirname + "/views/index.html")
-)})
+)});
 
+app.get("/login",(req,res) => {
+    res.sendFile(path.resolve(__dirname + "/views/login.html")
+)});
+
+app.get("/productCart",(req,res) => {
+    res.sendFile(path.resolve(__dirname + "/views/productCart.html")
+)});
+
+app.get("/productDetail",(req,res) => {
+    res.sendFile(path.resolve(__dirname + "/views/productDetail.html")
+)});
+
+app.get("/register",(req,res) => {
+    res.sendFile(path.resolve(__dirname + "/views/register.html")
+)});
+
+// app.get para levantar el servidor
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando 3000');
