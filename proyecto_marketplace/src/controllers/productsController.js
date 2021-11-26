a1b5b802a4979e725b7362fd25193c15ef48c02f
NicoADavite <nicolasalceodavite@gmail.com>
2021-11-25 23:27:46 -0300

    Terminamos de definir los archivos de las vistas parciales y su implementacion en las distintas vistas. Tambien empezamos a definir el archivo de creacion de productos

--- proyecto_marketplace/src/controllers/productsController.js
+++ proyecto_marketplace/src/controllers/productsController.js
@@ -7,6 +7,9 @@ const productsController = {
     productDetail: (req, res) => {
         res.render("./products/productDetail");
     },
+    productCreationEdit: (req, res) => {
+        res.render("./products/productCreationEdit");
+    },
 };
 
 module.exports = productsController;
