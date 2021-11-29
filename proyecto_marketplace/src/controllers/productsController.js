af9f4c0950ec761b1ac7cd81c14379c03efa0312
Fefaia <bianchi.fed@gmail.com>
2021-11-29 11:44:25 -0300

    Se actualizó el productDetail.ejs en función de lo solicitado en el SPRINT y, para ello,  se actualizó productsController.js y productsRouter.js

--- proyecto_marketplace/src/controllers/productsController.js
+++ proyecto_marketplace/src/controllers/productsController.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const productList = require("../database/productList")
 
 const productsController = {
     productCart: (req, res) => {
         res.render("./products/productCart");
     },
     productDetail: (req, res) => {
-        res.render("./products/productDetail");
+        let product = productList.find(product => product.id == req.params.productId);
+        console.log(product)
+        res.render("./products/productDetail", {product: product})
     },
     productCreation: (req, res) => {
         res.render("./products/productCreation");
--- proyecto_marketplace/src/routes/productsRouter.js
+++ proyecto_marketplace/src/routes/productsRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const productsController = require("../controllers/productsController");
 
-router.get("/productDetail", productsController.productDetail);
+router.get("/productDetail/:productId", productsController.productDetail);
 router.get("/productCart", productsController.productCart);
 router.get("/productCreation", productsController.productCreation);
 router.get("/productEdit", productsController.productEdit);
