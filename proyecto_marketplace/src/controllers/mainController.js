b1dc80cee1ae2102ce2acf5bd2582c0da239691e
NicoADavite <nicolasalceodavite@gmail.com>
2021-12-07 00:21:59 -0300

    Empezamos a configurar el proyecto para que responda al modelo CRUD

--- proyecto_marketplace/src/controllers/mainController.js
+++ proyecto_marketplace/src/controllers/mainController.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const fs = require('fs');
+const path = require("path");
 
-const productList = require("../database/productList")
+const productsFilePath = path.join(__dirname, '../data/products.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"))
+
 
 const mainController = {
     index: (req, res) => {
-        res.render("index", {product: productList});
+        res.render("index", {product: products});
     }
 }
 
